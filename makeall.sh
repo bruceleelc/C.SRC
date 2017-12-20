b054b0fb7b57e9c50957ecc26df19495d1a4317a
brucelee <brucelee@ACA80165.ipt.aol.com>
2017-12-20 16:16:16 +0800

    3ff

--- makeall.sh
+++ makeall.sh
@@ -2,34 +2,38 @@
 mkdir /home/abk/devstatusdeal
 cd /home/svn/C.SRC/devstatusdeal/
 make clean;make
-mv bin/* /home/abk/devstatusdeal/
+cp bin/* /home/abk/devstatusdeal/
 
 mkdir /home/abk/gpsalarmdeal
 cd /home/svn/C.SRC/gpsalarmdeal/
 make clean;make
-mv bin/* /home/abk/gpsalarmdeal/
+cp bin/* /home/abk/gpsalarmdeal/
 
 mkdir /home/abk/gpsdata
 cd /home/svn/C.SRC/gpsdata/
 make clean;make
-mv bin/* /home/abk/gpsdata/
+cp bin/* /home/abk/gpsdata/
 
 mkdir /home/abk/gpslocationdeal
 cd /home/svn/C.SRC/gpslocationdeal/
 make clean;make
-mv bin/* /home/abk/gpslocationdeal/
+cp bin/* /home/abk/gpslocationdeal/
 
 mkdir /home/abk/studentdevstatusdeal
 cd /home/svn/C.SRC/studentdevstatusdeal/
 make clean;make
-mv bin/* /home/abk/studentdevstatusdeal/
+cp bin/* /home/abk/studentdevstatusdeal/
 
 mkdir /home/abk/studentgpsdatarecv
 cd /home/svn/C.SRC/studentgpsdatarecv/
 make clean;make
-mv bin/* /home/abk/studentgpsdatarecv/
+cp bin/* /home/abk/studentgpsdatarecv/
 
 mkdir /home/abk/studentgpslocationdeal
 cd /home/svn/C.SRC/studentgpslocationdeal/
 make clean;make
-mv bin/* /home/abk/studentgpslocationdeal/
+cp bin/* /home/abk/studentgpslocationdeal/
+
+cd /home/svn/C.SRC/
+cp killall.sh /home/abk/
+cp startall.sh /home/abk
